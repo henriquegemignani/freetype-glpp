12bee69a702298d4bbc34c26df30d689a96cd044
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-04-06 19:34:56 -0300

    Added general include.

---
+++ include/freetype-gl++/freetype-gl++.hpp
@@ -0,0 +1,11 @@
+// TODO: license text
+#ifndef FREETYPEGLXX_FREETYPEGLXX_HPP_
+#define FREETYPEGLXX_FREETYPEGLXX_HPP_
+
+/* Mandatory */
+#include <freetype-gl++/vec234.hpp>
+//#include "vector.h"
+#include <freetype-gl++/texture-atlas.hpp>
+//#include "texture-font.h"
+
+#endif // FREETYPEGLXX_FREETYPEGLXX_HPP_
