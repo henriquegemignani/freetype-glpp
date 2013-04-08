94e734003116961b332ca2e851ef90335302ce5a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-04-08 20:55:27 -0300

    Added VertexBuffer wrapper.

---
+++ include/freetype-gl++/vertex-buffer.hpp
@@ -0,0 +1,170 @@
+/* ============================================================================
+ * Freetype GL++ - A C++ FreeType-GL wrapper
+ * Platform:       Any
+ * WWW:            https://github.com/henriquegemignani/freetype-glpp
+ * ----------------------------------------------------------------------------
+ * Copyright 2013 Henrique Gemignani. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY HENRIQUE GEMIGNANI ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL HENRIQUE GEMIGNANI OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Henrique Gemignani.
+ * ============================================================================
+ * 
+ *  This library is merely a wrapper of the Freetype GL by Nicolas P. Rougier.
+ *  WWW: http://code.google.com/p/freetype-gl/
+ * 
+ * ============================================================================
+ */
+#ifndef FREETYPEGLXX_VERTEXBUFFER_H_
+#define FREETYPEGLXX_VERTEXBUFFER_H_
+
+#include <cstddef>
+#include <string>
+#include <freetype-gl++/opengl.hpp>
+
+namespace freetypeglxx {
+
+/**
+ * @file   vertex-buffer.hpp
+ *
+ * @defgroup vertex-buffer Vertex buffer
+ *
+ * @{
+ */
+
+class VertexBuffer {
+  public:
+    /**
+     * Creates an empty vertex buffer.
+     *
+     * @param  format a string describing vertex format.
+     * @return        an empty vertex buffer.
+     */
+    VertexBuffer(const std::string& format);
+
+    /**
+     * Deletes vertex buffer and releases GPU memory.
+     *
+     */
+    ~VertexBuffer();
+
+    /**
+     *  Returns the number of items in the vertex buffer
+     *
+     *  @return       number of items
+     */
+    std::size_t size() const;
+
+    /**
+     *  Returns vertex format
+     *
+     *  @return       vertex format
+     */
+    const char* format() const;
+
+    /**
+     * Print information about a vertex buffer
+     *
+     */
+    void Print();
+
+    /**
+     * Prepare vertex buffer for render.
+     *
+     * @param  mode  render mode
+     */
+    void RenderSetup(GLenum mode);
+
+    /**
+     * Finish rendering by setting back modified states
+     *
+     */
+    void RenderFinish();
+
+    /**
+     * Render vertex buffer.
+     *
+     * @param  mode  render mode
+     */
+    void Render(GLenum mode);
+
+    /**
+     * Render a specified item from the vertex buffer.
+     *
+     * @param  index index of the item to be rendered 
+     */
+    void RenderItem(std::size_t index);
+
+    /**
+     * Upload buffer to GPU memory.
+     *
+     */
+    void Upload();
+
+    /**
+     * Clear all items.
+     *
+     */
+    void Clear();
+
+    /**
+     * Append a new item to the collection.
+     *
+     * @param  vcount   number of vertices
+     * @param  vertices raw vertices data
+     * @param  icount   number of indices
+     * @param  indices  raw indices data
+     */
+    std::size_t PushBack(void * vertices, std::size_t vcount,  
+                         GLuint * indices, std::size_t icount);
+
+    /**
+     * Insert a new item into the vertex buffer.
+     *
+     * @param  index     location before which to insert item
+     * @param  vertices  raw vertices data
+     * @param  vcount    number of vertices
+     * @param  indices   raw indices data
+     * @param  icount    number of indices
+     */
+    std::size_t Insert(std::size_t index,
+                       void * vertices, std::size_t vcount,  
+                       GLuint * indices, std::size_t icount);
+
+    /**
+     * Erase an item from the vertex buffer.
+     *
+     * @param  index    index of the item to be deleted
+     */
+    void Erase(std::size_t index);
+
+/** @} */
+  private:
+    void* self_;
+
+};
+
+} // namespace freetypeglxx
+
+#endif // FREETYPEGLXX_VERTEXBUFFER_H_
---
+++ src/vertex-buffer.cpp
@@ -0,0 +1,105 @@
+/* ============================================================================
+ * Freetype GL++ - A C++ FreeType-GL wrapper
+ * Platform:       Any
+ * WWW:            https://github.com/henriquegemignani/freetype-glpp
+ * ----------------------------------------------------------------------------
+ * Copyright 2013 Henrique Gemignani. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY HENRIQUE GEMIGNANI ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL HENRIQUE GEMIGNANI OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Henrique Gemignani.
+ * ============================================================================
+ * 
+ *  This library is merely a wrapper of the Freetype GL by Nicolas P. Rougier.
+ *  WWW: http://code.google.com/p/freetype-gl/
+ * 
+ * ============================================================================
+ */
+#include <freetype-gl++/vertex-buffer.hpp>
+
+#include "vertex-buffer.h"
+
+#define CASTED_SELF static_cast<vertex_buffer_t *>(self_)
+
+namespace freetypeglxx {
+
+VertexBuffer::VertexBuffer(const std::string& format)
+    : self_(vertex_buffer_new(format.c_str())) {}
+
+VertexBuffer::~VertexBuffer() {
+    vertex_buffer_delete(CASTED_SELF);
+}
+
+std::size_t VertexBuffer::size() const {
+    return vertex_buffer_size(CASTED_SELF);
+}
+
+const char* VertexBuffer::format() const {
+    return vertex_buffer_format(CASTED_SELF);
+}
+
+void VertexBuffer::Print() {
+    vertex_buffer_print(CASTED_SELF);
+}
+
+void VertexBuffer::RenderSetup(GLenum mode) {
+    vertex_buffer_render_setup(CASTED_SELF, mode);
+}
+
+void VertexBuffer::RenderFinish() {
+    vertex_buffer_render_finish(CASTED_SELF);
+}
+
+void VertexBuffer::Render(GLenum mode) {
+    vertex_buffer_render(CASTED_SELF, mode);
+}
+
+void VertexBuffer::RenderItem(std::size_t index) {
+    vertex_buffer_render_item(CASTED_SELF, index);
+}
+
+void VertexBuffer::Upload() {
+    vertex_buffer_upload(CASTED_SELF);
+}
+
+void VertexBuffer::Clear() {
+    vertex_buffer_clear(CASTED_SELF);
+}
+
+std::size_t VertexBuffer::PushBack(void * vertices, std::size_t vcount,  
+                     GLuint * indices, std::size_t icount) {
+    return vertex_buffer_push_back(CASTED_SELF, vertices, vcount, indices, icount);
+}
+
+std::size_t VertexBuffer::Insert(std::size_t index,
+                   void * vertices, std::size_t vcount,  
+                   GLuint * indices, std::size_t icount) {
+    return vertex_buffer_insert(CASTED_SELF, index, vertices, vcount, indices, icount);
+}
+                    
+void VertexBuffer::Erase(std::size_t index) {
+    vertex_buffer_erase(CASTED_SELF, index);
+}
+
+} // namespace freetypeglxx
